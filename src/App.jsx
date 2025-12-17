a037eb5c80d40174bed1bd3641bdab68cf510ce4
Swastik <93513267+swastikr7@users.noreply.github.com>
2025-12-18 00:48:14 +0530

    Refactor routing structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-
+import { Routes, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
-// Dashboards
 import StudentDashboard from "./pages/StudentDashboard";
 import TeacherDashboard from "./pages/TeacherDashboard";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public */}
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+    <Routes>
+      {/* Public */}
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
 
-        {/* Dashboards */}
-        <Route
-          path="/student/dashboard"
-          element={<StudentDashboard />}
-        />
-        <Route
-          path="/teacher/dashboard"
-          element={<TeacherDashboard />}
-        />
+      {/* Dashboards */}
+      <Route path="/student/dashboard" element={<StudentDashboard />} />
+      <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
 
-        {/* Fallback */}
-        <Route path="*" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+      {/* Fallback */}
+      <Route path="*" element={<Landing />} />
+    </Routes>
   );
 }
+
